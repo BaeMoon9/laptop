75198bab47a3c32fb45a4ebbebe023d203a64b8f
BaeMoon9 <bmgbm9@naver.com>
2024-05-19 22:19:35 +0900

    123

--- server/rentlaptop/src/pages/findid.js
+++ server/rentlaptop/src/pages/findid.js
@@ -1,52 +1,43 @@
 import React, { useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
+import Navbarpage from "./navpage.js";
 
 function FindID() {
 
 	const navigate = useNavigate();
-	const [userData, setUserData] = useState([])
 
 
 	return (
-		<div className="registerpage">
-			<div className="registertitle">
-				아이디 찾기
-			</div>
-			<hr className="divider" />
-			<div className="registercontent">
-				<form method="POST">
-					<div className="registersubtitle">
-						학번
-					</div>
-					<div className="registerid">
-						<input name="userstudentnum" type="text" className="registerinput"
-							placeholder="학번을 입력해주세요." value={userData}
-							onChange={(e) => {
-								setUserData(e.target.value)
-								console.log(userData)
-							}}
-						/>
-					</div>
-					<div className="registersubtitle">
-						이름
-					</div>
-					<div className="registerid">
-						<input name="userstudentnum" type="text" className="registerinput"
-							placeholder="이름을 입력해주세요." value={userData}
-							onChange={(e) => {
-								setUserData(e.target.value)
-								console.log(userData)
-							}}
-						/>
-						<button className="findidBtn" type="submit">확인하기</button>
-					</div>
-				</form>
+		<div>
+			<Navbarpage />
+			<div className="registerpage">
+				<div className="registertitle">
+					아이디 찾기
+				</div>
 				<hr className="divider" />
-				<button className="findidBtn" type="button" onClick={() => navigate('/login')}>로그인하기</button>
+				<div className="registercontent">
+					<form action="http://localhost:8081/findyourid" method="POST" >
+						<div className="registersubtitle">
+							학번
+						</div>
+						<div className="registerid">
+							<input name="userstudentnum" type="text" className="registerinput"
+								placeholder="학번을 입력해주세요." />
+						</div>
+						<div className="registersubtitle">
+							이름
+						</div>
+						<div className="registerid">
+							<input name="userstudentnum" type="text" className="registerinput"
+								placeholder="이름을 입력해주세요." />
+							<button className="findidBtn" type="submit">확인하기</button>
+						</div>
+					</form>
+					<hr className="divider" />
+					<button className="findidBtn" type="button" onClick={() => navigate('/login')}>로그인하기</button>
+				</div>
 			</div>
-
-
 		</div>
 	)
 }
