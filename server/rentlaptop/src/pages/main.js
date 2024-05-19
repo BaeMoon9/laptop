a05c562bd602a7f064ea67c0d2cbb652d6c2e19c
BaeMoon9 <bmgbm9@naver.com>
2024-05-19 16:52:55 +0900

    navbar css

--- server/rentlaptop/src/pages/main.js
+++ server/rentlaptop/src/pages/main.js
@@ -44,24 +44,37 @@ function MainPage() {
 
 	return (
 		<div className="main">
-			<Navbar bg="dark" data-bs-theme="dark">
+			<Navbar bg="light" data-bs-theme="light">
 				<Container>
-					<Navbar.Brand onClick={() => { navigate('/') }}>노트북 관리 대장</Navbar.Brand>
+					<Navbar.Brand onClick={() => { navigate('/') }}>
+						<div className="navtitle">
+							<img className="ynclogo2" src="./ynclogo1.ico" />
+							소프트웨어융합과 노트북
+						</div>
+					</Navbar.Brand>
 					<Nav className="me-auto">
-						<Nav.Link onClick={() => { navigate('/home') }}>Home</Nav.Link>
+						<Nav.Link onClick={() => { navigate('/home') }}>
+							<div className="navtabs">Home</div>
+						</Nav.Link>
 						{
 							!userData.id
 								? null
 								: (userData.id === 'admin'
-									? <Nav.Link onClick={() => { navigate('/list') }}>노트북 목록</Nav.Link>
+									? <Nav.Link onClick={() => { navigate('/list') }}>
+										<div className="navtabs">노트북 목록</div>
+									</Nav.Link>
 									: null)
 						}
 						{
 							!userData.id
 								? null
 								: (userData.id === 'admin'
-									? <Nav.Link onClick={() => { navigate('/approve') }}>신청자 목록</Nav.Link>
-									: <Nav.Link onClick={() => { handleUserData({ userData }) }}>신청 현황</Nav.Link>)
+									? <Nav.Link onClick={() => { navigate('/approve') }}>
+										<div className="navtabs">신청자 목록</div>
+									</Nav.Link>
+									: <Nav.Link onClick={() => { handleUserData({ userData }) }}>
+										<div className="navtabs">신청 현황</div>
+									</Nav.Link>)
 						}
 					</Nav>
 					<Navbar.Toggle />
@@ -69,8 +82,8 @@ function MainPage() {
 						<Navbar.Text>
 							{
 								!userData.id
-									? <p className="logintab"><a onClick={() => { navigate('/login') }}>로그인 하러가기</a>!</p>
-									: <p className="logintab">환영합니다 <a onClick={() => { navigate('/mypage') }}>{userData.id}</a> 님!</p>
+									? <p className="logintab"><a className="navlogin" onClick={() => { navigate('/login') }}>로그인 하러가기</a>!</p>
+									: <p className="logintab"><a className="navlogin" onClick={() => { navigate('/mypage') }}>환영합니다 {userData.id}님!</a></p>
 							}
 						</Navbar.Text>
 					</Navbar.Collapse>
@@ -102,8 +115,8 @@ function MainPage() {
 						영남이공대학교
 					</div>
 					<p>대구광역시 남구 현충로 170(대명동) 영남이공대학교 컴퓨터정보관3층 실습준비실3 Tel : 053-650-9255
-							<br/>
-							Copyrightⓒ 2023 Yeungnam University College All Rights Reserved
+						<br />
+						Copyrightⓒ 2023 Yeungnam University College All Rights Reserved
 					</p>
 				</div>
 			</div>
