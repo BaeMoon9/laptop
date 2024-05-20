3cef3b3773b9f42fde6c7a58f1b22ef7ec02031f
BaeMoon9 <bmgbm9@naver.com>
2024-05-20 11:45:40 +0900

    findid

--- server/rentlaptop/src/pages/findid.js
+++ server/rentlaptop/src/pages/findid.js
@@ -7,7 +7,30 @@ function FindID() {
 
 	const navigate = useNavigate();
 
+	const [findStuNum, setFindStuNum] = useState();
+	const [findStuName, setFindStuName] = useState();
 
+	const findID = async (stunum, stuname) => {
+		let db1 = [...stunum, ...stuname]
+
+		await axios.post('http://localhost:8081/findyourid', db1, {
+			headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+		})
+			.then((res) => {
+				console.log('res', res.data[0])
+				if(res.data[0] = '') {
+					alert('찾으시려는 아이디가 없습니다.')
+				} else if (res.data[0]) {
+					alert('아이디찾음')
+				} else {
+					alert('관리자에게 문의바랍니다.')
+				}
+			}).catch((e) => {
+				console.log(e)
+			})
+		
+
+	}
 	return (
 		<div>
 			<Navbarpage />
@@ -17,25 +40,31 @@ function FindID() {
 				</div>
 				<hr className="divider" />
 				<div className="registercontent">
-					<form action="http://localhost:8081/findyourid" method="POST" >
 						<div className="registersubtitle">
 							학번
 						</div>
 						<div className="registerid">
 							<input name="userstudentnum" type="text" className="registerinput"
-								placeholder="학번을 입력해주세요." />
+								placeholder="학번을 입력해주세요." 
+								onChange={(e) => {
+									setFindStuNum(e.target.value)
+								}}
+								/>
 						</div>
 						<div className="registersubtitle">
 							이름
 						</div>
 						<div className="registerid">
-							<input name="userstudentnum" type="text" className="registerinput"
-								placeholder="이름을 입력해주세요." />
-							<button className="findidBtn" type="submit">확인하기</button>
+							<input name="userstudentname" type="text" className="registerinput"
+								placeholder="이름을 입력해주세요." 
+								onChange={(e) => {
+									setFindStuName(e.target.value)
+								}}
+								/>							
+							<button className="findidBtn" type="button" onClick={() => findID([findStuNum], [findStuName])}>확인하기</button>
 						</div>
-					</form>
 					<hr className="divider" />
-					<button className="findidBtn" type="button" onClick={() => navigate('/login')}>로그인하기</button>
+					<button className="doneBtn" type="button" onClick={() => navigate('/login')}>로그인하기</button>
 				</div>
 			</div>
 		</div>
